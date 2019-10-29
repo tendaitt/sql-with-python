7a635405f18cf37bef9c15f336374828058ff262
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-10-29 12:58:13 +0200

    added error handling.

--- 02_sql.py
+++ 02_sql.py
@@ -1,4 +1,4 @@
-# INSERT Command
+# INSERT Command with Error Handler
 
 # import the sqlite3 library
 import sqlite3
@@ -9,14 +9,17 @@ conn = sqlite3.connect("new.db")
 # get a cursor object used to execute SQL commands
 cursor = conn.cursor()
 
-# insert data
-cursor.execute("INSERT INTO population VALUES('New York City', \
-    'NY', 8400000)")
-cursor.execute("INSERT INTO population VALUES('San Francisco', \
-    'CA', 800000)")
+try:
+    # insert data
+    cursor.execute("INSERT INTO population VALUES('New York City', \
+        'NY', 8400000)")
+    cursor.execute("INSERT INTO population VALUES('San Francisco', \
+        'CA', 800000)")
 
-# commit the changes
-conn.commit()
+    # commit the changes
+    conn.commit()
+except sqlite3.OperationalError:
+    print("Oops! Something went wrong. Try again...")
 
 # close the database connection
 conn.close()
